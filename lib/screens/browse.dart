d5f2432b2c0892a76bbfaa8bb8e8c23a421001d2
Talin Keshesh <talin.keshesh@gmail.com>
2025-11-28 19:00:11 -0800

    revert test

--- lib/screens/browse.dart
+++ lib/screens/browse.dart
@@ -104,7 +104,7 @@ void _onCategoryTap(String id, String label) {
             // 🔍 Search Bar (UI only for now)
             TextField(
               decoration: InputDecoration(
-                hintText: "Search people or skills... (coming soon123)",
+                hintText: "Search people or skills... (coming soon)",
                 hintStyle: TextStyle(
                   color: Colors.grey.shade600,
                   fontSize: 15,
